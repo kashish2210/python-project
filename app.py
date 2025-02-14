6fab541a2d2da2fa394d0ebd8d7218b010bf6bb6
kashish2210 <153910815+kashish2210@users.noreply.github.com>
2025-02-14 06:23:14 +0530

    ss

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import email_validator
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail, Message
 import os
-app = Flask(__name__, static_url_path='/static', static_folder='static')
+from whitenoise import WhiteNoise
+
+app = Flask(__name__, static_folder='static')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 Bootstrap(app)
 app.secret_key = "kashish2210"
 
